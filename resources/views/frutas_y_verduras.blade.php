4dbef02c96dd43cf53d5cc9c440ca6f2805fc024
smenaq <smenaq@unsa.edu.pe>
2022-12-23 11:19:34 -0500

    app(.js and .css)

--- resources/views/frutas_y_verduras.blade.php
+++ resources/views/frutas_y_verduras.blade.php
@@ -18,7 +18,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/products.css') }}">
-    <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/main.js') }}"></script>
     <title>Frutas y Verduras</title>
 </head>
 
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -11,12 +11,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/app.css ')}}">
+    <link rel="stylesheet" href="{{ asset('css/main.css ')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/release/v5.6.3/css/all.css">
     <script src="https://kit.fontawesome.com/887a835504.js" crossorigin="anonymous"></script>
 
-    <script type="text/javascript" src="{{ asset('js/app.js')}}"></script>
+    <script type="text/javascript" src="{{ asset('js/main.js')}}"></script>
 
     </header>
     <title>El Aprobado</title>
